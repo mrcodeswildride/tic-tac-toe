37b793d216e52de0d1a035303fef10d123f0e14e
David Keller <kellerdb@gmail.com>
2021-05-11 14:04:09 -0700

    formatting

--- script.js
+++ script.js
@@ -15,40 +15,63 @@ function clickSquare() {
     if (threeInRow()) {
       messageParagraph.innerHTML = `${turn} wins!`
       gameOver = true
-    }
-    else if (boardIsFull()) {
+    } else if (boardIsFull()) {
       messageParagraph.innerHTML = `Tie game`
       gameOver = true
-    }
-    else {
+    } else {
       turn = turn == `X` ? `O` : `X`
     }
   }
 }
 
 function threeInRow() {
-  if (squares[0].innerHTML == turn && squares[1].innerHTML == turn && squares[2].innerHTML == turn) {
+  if (
+    squares[0].innerHTML == turn &&
+    squares[1].innerHTML == turn &&
+    squares[2].innerHTML == turn
+  ) {
     return true
-  }
-  else if (squares[3].innerHTML == turn && squares[4].innerHTML == turn && squares[5].innerHTML == turn) {
+  } else if (
+    squares[3].innerHTML == turn &&
+    squares[4].innerHTML == turn &&
+    squares[5].innerHTML == turn
+  ) {
     return true
-  }
-  else if (squares[6].innerHTML == turn && squares[7].innerHTML == turn && squares[8].innerHTML == turn) {
+  } else if (
+    squares[6].innerHTML == turn &&
+    squares[7].innerHTML == turn &&
+    squares[8].innerHTML == turn
+  ) {
     return true
-  }
-  else if (squares[0].innerHTML == turn && squares[3].innerHTML == turn && squares[6].innerHTML == turn) {
+  } else if (
+    squares[0].innerHTML == turn &&
+    squares[3].innerHTML == turn &&
+    squares[6].innerHTML == turn
+  ) {
     return true
-  }
-  else if (squares[1].innerHTML == turn && squares[4].innerHTML == turn && squares[7].innerHTML == turn) {
+  } else if (
+    squares[1].innerHTML == turn &&
+    squares[4].innerHTML == turn &&
+    squares[7].innerHTML == turn
+  ) {
     return true
-  }
-  else if (squares[2].innerHTML == turn && squares[5].innerHTML == turn && squares[8].innerHTML == turn) {
+  } else if (
+    squares[2].innerHTML == turn &&
+    squares[5].innerHTML == turn &&
+    squares[8].innerHTML == turn
+  ) {
     return true
-  }
-  else if (squares[0].innerHTML == turn && squares[4].innerHTML == turn && squares[8].innerHTML == turn) {
+  } else if (
+    squares[0].innerHTML == turn &&
+    squares[4].innerHTML == turn &&
+    squares[8].innerHTML == turn
+  ) {
     return true
-  }
-  else if (squares[2].innerHTML == turn && squares[4].innerHTML == turn && squares[6].innerHTML == turn) {
+  } else if (
+    squares[2].innerHTML == turn &&
+    squares[4].innerHTML == turn &&
+    squares[6].innerHTML == turn
+  ) {
     return true
   }
 
